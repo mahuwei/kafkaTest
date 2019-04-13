530ee855a8bf699ddf925bfb1438a5b5d5ce8ff4
mahuwei <mahuwei@msn.com>
2019-04-13 16:43:22 +0800

    记录错误

---
+++ Client/KafkaClent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+
+namespace Client {
+    public class KafkaClent {
+        private readonly KafkaOptions _options;
+        private readonly BrokerRouter _router;
+        private const string CsTopic = "test_topic";
+
+        public KafkaClent() {
+            _options = new KafkaOptions(new Uri("http://192.168.1.104:9092"));
+            _router = new BrokerRouter(_options);
+        }
+
+        public void Producer(string msg) {
+            using (Producer client = new Producer(_router)) {
+                client.SendMessageAsync(CsTopic, new[] { new Message(msg) }).Wait();
+            }
+        }
+
+        public void Consumer() {
+            using (var consumer = new Consumer(new ConsumerOptions(CsTopic, _router))) {
+                // Consume returns a blocking IEnumerable (ie: never ending stream)
+                foreach (var message in consumer.Consume()) {
+                    Console.WriteLine(" Response: P{0},O{1} : {2}",
+                        message.Meta.PartitionId, message.Meta.Offset, message.Value);
+                }
+            }
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,70 @@
 ﻿using System;
+using System.Text;
 
 namespace Client {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+    internal class Program {
+        private const string CsHelp = "help";
+        private const string CsQuit = "quit";
+        private const string CsSend = "send";
+        private const string CsReceive = "receive";
+        private static string _helpInfo;
+        private static void Main(string[] args) {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("kafka客户端测试：\n");
+            sb.Append($" {CsHelp}:显示帮助信息\n");
+            sb.Append(" quit:退出\n");
+            sb.Append($" {CsSend} string:发送消息\n");
+            sb.Append($" {CsReceive}:接收消息消息\n");
+            sb.Append("---------结束------\n");
+            _helpInfo = sb.ToString();
+
+            KafkaClent kc = new KafkaClent();
+            Console.WriteLine(_helpInfo);
+            bool isContinue = true;
+            do {
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) {
+                    continue;
+                }
+                input = input.Replace("  ", " ");
+                string[] datas = input.Split(' ');
+                string cmd = datas[0].ToLower();
+                switch (cmd) {
+                    case CsHelp:
+                        Console.WriteLine(_helpInfo);
+                        break;
+                    case CsQuit:
+                        isContinue = false;
+                        break;
+                    case CsSend:
+                        if (datas.Length == 1) {
+                            Console.WriteLine("没有指定要发送的数据.");
+                            continue;
+                        }
+                        try {
+                            kc.Producer(input.Substring(CsQuit.Length + 1));
+                            Console.WriteLine("消息发送成功。");
+                        }
+                        catch (Exception ex) {
+                            Console.WriteLine("发送消息发生错误：{0}", ex.Message);
+                        }
+                        break;
+                    case CsReceive:
+                        try {
+                            Console.WriteLine("接收消息------开始-----");
+                            kc.Consumer();
+                            Console.WriteLine("接收消息------结束-----");
+                        }
+                        catch (Exception ex) {
+                            Console.WriteLine("接收消息发生错误：{0}", ex.Message);
+                        }
+                        break;
+                    default:
+                        continue;
+                }
+
+            } while (isContinue);
+
         }
     }
 }
